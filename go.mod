b841f1d6a9bd5ff9dd522d849e86e82760382431
Ralf Rantzau <rrantzau1@gmail.com>
2021-08-21 16:12:11 -0700

    add test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xralf/hello
 
 go 1.16
+
+require github.com/google/go-cmp v0.5.6
--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/xralf/hello/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
